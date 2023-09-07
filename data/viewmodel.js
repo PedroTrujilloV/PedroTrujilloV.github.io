79072203f3d5be163ead05f6f159594a404d79ea
PedroTrujilloV <pedrot211@gmail.com>
2023-09-06 23:02:09 -0500

    refactor: Updated resume.json file and viewmodel

--- data/viewmodel.js
+++ data/viewmodel.js
@@ -1,33 +1,13 @@
-
-// var mydata = JSON.parse(data);
-// $.ajax({
-//     url: "https://type.fit/api/quotes",
-//     method: "GET"
-//   }).then(function(data) {
-//     data = JSON.parse(data);
-    
-//     $("#text").text(data[1].text);
-//     $("#author").text(data[1].author);
-//   });
-
-
-//   $.getJSON("data.json", function(json) {
-//     console.log(json); // this will show the info it in firebug console
-// }).then(function(data) {
-//     data = JSON.parse(data);
-    
-//     document.write(data);
-//     // $("#text").text(data[1].text);
-//     // $("#author").text(data[1].author);
-// });
-
-fetch('./data/resume.json')
+fetch('https://raw.githubusercontent.com/PedroTrujilloV/PedroTrujilloV.github.io/main/data/resume.json')
     .then(response => response.json())
     .then(function(data) {
-        data = JSON.parse(data);
         console.log("- data: "); 
         console.log(data); 
-        document.write(data);
+        // document.write(data);
+        Object.entries(data).forEach(([key, value]) => {
+            console.log(key, value);
+         });
         // $("#text").text(data[1].text);
         // $("#author").text(data[1].author);
     })
+    .catch(error => console.log(error));
